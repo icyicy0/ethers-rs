a106bc34d14c186e89c314748c2f4a09639e8111
icyicy0 <timoleon.demas@etu.umontpellier.fr>
2021-08-16 16:20:29 +0200

    implementing flashbots

--- ethers-middleware/src/flashbots/bundle.rs
+++ ethers-middleware/src/flashbots/bundle.rs
@@ -1,4 +1,4 @@
-use crate::utils::{deserialize_u256, deserialize_u64};
+use crate::flashbots::utils::{deserialize_u256, deserialize_u64};
 use ethers_core::{
     types::{transaction::response::Transaction, Address, Bytes, H256, U256, U64},
     utils::keccak256,
